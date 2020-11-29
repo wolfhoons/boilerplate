13d8e4cc43ba3e0ae025f9e54aa53bf6b069e60f
wolfhoons <70469853+wolfhoons@users.noreply.github.com>
2020-11-30 02:05:07 +0900

    register,login,logout

--- server/index.js
+++ server/index.js
@@ -24,6 +24,8 @@ mongoose.connect(config.mongoURI,
 
 app.get('/', (req, res) => res.send("Hello"));
 
+app.get('/api/hello', (req, res) => res.send('안녕하세요'))
+
 app.post("/api/users/register", (req, res) => {
     // 회원 가입 할 때 필요한 정보를 Client에서 가져오면
     // 그것들을 데이터 베이스에 넣어준다.
@@ -39,41 +41,40 @@ app.post("/api/users/register", (req, res) => {
     })
 })
 
-app.post("/api/users/login", (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
-    // 요청된 이메일을 DB에 있는지 찾는다.
-    User.findOne({ email: req.body.email }, (err, user) => {
+  // console.log('ping')
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
 
-        console.log('user', user)
-        if (!user) {
-            return res.json({
-                loginSuccess: false,
-                message: "제공된 이메일에 해당하는 유저가 없습니다."
-            })
-        }
+    // console.log('user', user)
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
 
-        // 요청된 이메일이 DB에 있다면 비밀번호가 맞는지 확인
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if (!isMatch)
-                return res.json({
-                    loginSuccess: false,
-                    message: "비밀번호가 틀렸습니다."
-                })
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      // console.log('err',err)
 
-            // 비밀번호가 맞다면 토큰을 생성한다.
-            user.generateToken((err, user) => {
-                if (err) return res.status(400).send(err);
+      // console.log('isMatch',isMatch)
 
-                // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
-                res.cookie("x__auth", user.token)
-                    .status(200)
-                    .json({
-                        loginSuccess: true,
-                        userId: user._id
-                    })
-            })
-        })
-    });
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+
+      //비밀번호 까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다.  어디에 ?  쿠키 , 로컳스토리지 
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+      })
+    })
+  })
 })
 
 app.get("/api/users/auth", auth ,(req, res) => {
@@ -92,7 +93,8 @@ app.get("/api/users/auth", auth ,(req, res) => {
 })
 
 app.get("/api/users/logout", auth, (req, res) => {
-    User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, 
+    User.findOneAndUpdate({ _id: req.user._id }, 
+      { token: "" }, 
     (err, user) => {
         if (err) return res.json({ success: false, err });
         return res.status(200).send({ success: true })
